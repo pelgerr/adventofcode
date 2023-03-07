57bf0a7cb3aace2c5d3f3a9d8c314a898af823c6
Onigiri <4869909+onigiri070@users.noreply.github.com>
2023-03-07 08:46:18 -0500

    Added smaller set of test data

--- src/2022/d1/d1.cpp
+++ src/2022/d1/d1.cpp
@@ -8,39 +8,52 @@ int main()
 {
     std::ifstream ifs;
 
-    ifs.open("input.txt", std::ios::in);
-
+    ifs.open("input-test.txt", std::ios::in);
     if (ifs)
     {
         std::string line;
-        std::vector<int> topCal = {0};
+        std::vector<int> topCal;
+        int topThree[3] = {0, 0, 0};
         int sum = 0;
         int high = 0;
+
         while (!ifs.eof()) {
             std::getline(ifs, line);
             if (!line.empty())
             {
                 sum += std::stoi(line);             
-                // std::cout << sum << "\n";
             }
             else 
             {
                 topCal.push_back(sum);
-                //std::cout << sum << "\n\n";
                 sum = 0;
-                // std::cout << sum << "\n\n";
             }
         }
+        sum = 0;
         // EOF reached
         for (int i = 0; i <= topCal.size(); i++)
         {
-            //std::cout << topCal[i] << "\n";
-            if (topCal[i] > high)
+            std::cout << "topCal: " << topCal[i] << std::endl;
+            if (topCal[i] > topThree[0])
             {
-                high = topCal[i];
+                topThree[0] = topCal[i];
+            }
+            else if (topCal[i] < topThree[0] && topCal[i] > topThree[1])
+            {
+                topThree[1] = topCal[i];
+            }
+            else if (topCal[i] < topThree[1] && topCal[i] > topThree[2])
+            {
+                topThree[2] = topCal[i];
             }
         }
-        std::cout << high;
+        for (int j : topThree)
+        {
+            std::cout << "Printing topThree array:" << std::endl;
+            std::cout << j << std::endl;
+            sum += j;
+        }
+        std::cout << "Sum: " << sum << std::endl;
     }
     ifs.close();
 }
