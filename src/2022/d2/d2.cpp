84e15f6fe695d207044631bc805fd5c1eb3d5a8b
Onigiri <4869909+onigiri070@users.noreply.github.com>
2023-03-10 23:24:55 -0500

    Inital Day 2 push; unfinished

---
+++ src/2022/d2/d2.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ifstream input_file_stream;
+
+    input_file_stream.open("input-test.txt", std::ios::in);
+    if (input_file_stream) {
+        std::string input_line;
+
+        while (!input_file_stream.eof()) {
+            std::getline(input_file_stream, input_line);
+            /* TODO:
+             * - Find winner of each line
+             * - Determine points won for each line
+             * - Sum total points won
+             */
+        }
+    }
+
+    return 1;
+}
